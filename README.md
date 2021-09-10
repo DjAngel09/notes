b122b9bc3f36d7c93cfa658b82ba3030f1124a77
Angel <angel@iMac-Pro-de-Angel.local>
2021-09-10 10:42:00 -0500

    se agregan algunas validaciones extra

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+
+# Notas
+
+Esta prueba fue creada con angular 12, firebase y bootstrap.
+
+# Funcionalidad
+
+crear, borrar, actualizar, archivar y desarchivar notas
+
+ligin con correo y google, logout
+
+El aplicativo es completamente responsive adapatado a escritorio, tablets y moviles
+
+Todas las notas estas asociadas a un usuario.
+
 # Notes
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.4.
@@ -25,3 +40,5 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
--- src/app/components/create-note/create-note.component.ts
+++ src/app/components/create-note/create-note.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { noteInterface } from 'src/app/interfaces/note-form-interface';
 import { NotesService } from '../../services/notes.service';
@@ -13,15 +13,15 @@ export class CreateNoteComponent implements OnInit, OnChanges {
 
   private colletionNote: AngularFirestoreCollection<noteInterface>;
   @Input() id: string = '';
-  @Input() uid: string |  undefined = '';
+  @Input() uid: string | undefined = '';
 
-  public noteForm:any;
+  public noteForm: FormGroup = new FormGroup({});
 
-  constructor( 
-    private fb: FormBuilder, 
-    private db: AngularFirestore, 
+  constructor(
+    private fb: FormBuilder,
+    private db: AngularFirestore,
     private _noteService: NotesService
-  ) { 
+  ) {
 
     this.colletionNote = db.collection<noteInterface>('notes');
     this.createForm();
@@ -29,30 +29,48 @@ export class CreateNoteComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
-    
+
   }
 
-  ngOnChanges() {    
-    if(this.id !== ''){
+  notEmailValidator(title: string) {
+
+    return (formGroup: FormGroup) => {
+
+      const titleControl = formGroup.get(title);
+      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(titleControl?.value)) {
+        titleControl?.setErrors(null)
+      } else {
+        titleControl?.setErrors({ isEmail: true });
+      }
+
+    }
+
+  }
+
+  ngOnChanges() {
+    if (this.id !== '') {
       this.editNote();
-    }else{
+    } else {
       this.createForm();
-    
+
     }
   }
 
-  createForm(){
+  createForm() {
     this.noteForm = this.fb.group({
       iduser: [this.uid],
-      id:[this.db.createId()],
-      title: ['', [ Validators.required]],
+      id: [this.db.createId()],
+      title: ['', [Validators.required]],
       content: ['', [Validators.required]],
       archived: [false, [Validators.required]],
       date: [new Date().getTime() / 1000, [Validators.required]]
+    }, {
+      validators: this.notEmailValidator('title')
     });
   }
 
-  editNote(){
+  editNote() {
     this.colletionNote.doc(this.id).valueChanges().subscribe((data) => {
       this.noteForm.setValue({
         ...data
@@ -60,14 +78,20 @@ export class CreateNoteComponent implements OnInit, OnChanges {
     })
   }
 
-  createNote(){
-    const note: noteInterface = this.noteForm.value;        
-    this._noteService.createNote(note, (res:any)=>{
-      this.createForm();
-      const modal:HTMLElement= document.getElementById('noteModal') as HTMLElement;
-      modal.click();
+  createNote() {
 
-    });
+    const note: noteInterface = this.noteForm.value;
+
+    if (this.noteForm.valid) {
+
+      this._noteService.createNote(note, (res: any) => {
+        this.createForm();
+        const modal: HTMLElement = document.getElementById('noteModal') as HTMLElement;
+        modal.click();
+
+      });
+      
+    }
   }
 
 }
