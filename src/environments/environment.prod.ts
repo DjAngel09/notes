2cac9ea58da979b5bef296071e44ce6ec5c1813a
DjAngel09 <33494755+DjAngel09@users.noreply.github.com>
2021-09-13 23:23:25 -0500

    changes
    firebase prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCt21Eg7fbl1YnjWKWsqQ8sRG6QV8ci470",
+    authDomain: "personal-notes-8e457.firebaseapp.com",
+    projectId: "personal-notes-8e457",
+    storageBucket: "personal-notes-8e457.appspot.com",
+    messagingSenderId: "648287147900",
+    appId: "1:648287147900:web:722169e2099a8ec53ff6f9",
+    measurementId: "G-517PYDG3GC"
+  }
 };
